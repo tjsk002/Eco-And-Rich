faf2d5db4933f86ac358acc5be9229317b77ba3a
tjsk002 <tjsk002@naver.com>
2024-12-05 21:56:08 +0900

    Initial commit

---
+++ src/test/java/com/ecoandrich/ecoandrich/EcoandrichApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecoandrich.ecoandrich;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoandrichApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
