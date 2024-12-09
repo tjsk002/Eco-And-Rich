f5b658663ed939580b0418afbcf85e4f57bc0a4b
tjsk002 <tjsk002@naver.com>
2024-12-09 16:46:41 +0900

    job entity add

---
+++ src/main/java/com/ecoandrich/ecoandrich/entity/Job.java
@@ -0,0 +1,26 @@
+package com.ecoandrich.ecoandrich.entity;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Data
+@Table(name = "jobs")
+public class Job {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "job_id")
+    private int jobId;
+
+    @Column(name = "job_title")
+    private String jobTitle;
+
+    @Column(name = "min_salary")
+    private BigDecimal minSalary;
+
+    @Column(name = "max_salary")
+    private BigDecimal maxSalary;
+}
